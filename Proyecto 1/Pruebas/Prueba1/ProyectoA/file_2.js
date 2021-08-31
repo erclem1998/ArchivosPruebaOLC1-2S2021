c4b66d6af4c4c5e5f15f96b181a399e9c71fb982
Herlindo René Corona Arenales <67886042+uhcorona@users.noreply.github.com>
2021-08-31 10:58:10 -0600

    Se agrego llaves

--- Proyecto 1/Pruebas/Prueba1/ProyectoA/file_2.js
+++ Proyecto 1/Pruebas/Prueba1/ProyectoA/file_2.js
@@ -14,8 +14,9 @@ class ejercicios_numericos {
     }
 
     torresDeHanoi(hanoi_generator_n, hanoi_generator_origin, hanoi_generator_aux, hanoi_generator_dest) {
-        if (hanoi_generator_n == 1)
+        if (hanoi_generator_n == 1){
             console.log("Mover disco de " + hanoi_generator_origin + " a " + hanoi_generator_dest);
+	}
         else {
             torresDeHanoi(hanoi_generator_n - 1, hanoi_generator_origin, hanoi_generator_dest, hanoi_generator_aux);
             console.log("mover disco de " + hanoi_generator_origin + " a " + hanoi_generator_dest);
